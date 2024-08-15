140b892563e0c2972bad584aec81cc708946f26c
Uphade Shrikant Bhagwat <uphadeshrikant712@gmail.com>
2024-08-15 22:47:47 +0530

    Alternate Text Change using SetInterval

--- 2.Tasks/src/components/20.Alternate Text Change/TextChange.jsx
+++ 2.Tasks/src/components/20.Alternate Text Change/TextChange.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const TextChange = () => {
   const [text, setText] = useState("I am Software Engineer");
