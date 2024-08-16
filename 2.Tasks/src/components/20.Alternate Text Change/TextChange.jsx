d31afe8a385ce7744e50769f2dfe0e93ac2de4eb
Uphade Shrikant Bhagwat <uphadeshrikant712@gmail.com>
2024-08-16 20:15:48 +0530

    Alternate text change

--- 2.Tasks/src/components/20.Alternate Text Change/TextChange.jsx
+++ 2.Tasks/src/components/20.Alternate Text Change/TextChange.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+
+
 
 const TextChange = () => {
   const [text, setText] = useState("I am Software Engineer");
