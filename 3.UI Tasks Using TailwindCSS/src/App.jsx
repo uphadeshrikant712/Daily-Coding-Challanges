ede725e90de079dcde6ebaf3db3aa9f1b9108227
Uphade Shrikant Bhagwat <uphadeshrikant712@gmail.com>
2024-08-28 18:58:42 +0530

    3.UI Tasks Using TailwindCSS

---
+++ 3.UI Tasks Using TailwindCSS/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BgChanger from './components/1 BgChangerButtonOnHover/BgChanger'
+
+const App = () => {
+  return (
+    <>
+    <BgChanger/>
+    </>
+  )
+}
+
+export default App
---
+++ 3.UI Tasks Using TailwindCSS/src/components/1 BgChangerButtonOnHover/BgChanger.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const BgChanger = () => {
+  const [active, setActive] = useState('start');
+
+  const handleClick = (option) => {
+    setActive(option)
+  }
+  return (
+    <div className='flex items-center justify-center py-12 gap-1'>
+      
+      <button
+        onClick={() => handleClick('start')}
+        className={` px-7 py-6 rounded-t-md ease-in duration-300 transition-all ${active === 'start' ? 'bg-black text-white' : 'bg-indigo-700 text-white hover:bg-black'} `}>Start</button>
+
+      <button
+        onClick={() => handleClick('stop')}
+        className={` px-7 py-6 rounded-t-md text-white ease-in duration-300 transition-all ${active === 'stop' ? 'bg-black' : 'bg-indigo-600 hover:bg-black'}`}>Stop</button>
+    
+    </div>
+  )
+}
+
+export default BgChanger
